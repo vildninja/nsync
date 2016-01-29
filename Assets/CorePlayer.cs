95099dfa68e23061639608bedfd7385558380921
Jannek <jannek.ek@gmail.com>
2016-01-29 20:50:41 +0100

    initial

---
+++ Assets/CorePlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CorePlayer : MonoBehaviour
+{
+
+    private HFTInput input;
+
+    private Vector3 Joystick
+    {
+        get { return new Vector3(input.GetAxis("Horizontal"), input.GetAxis("Vertical")); }
+    }
+
+	// Use this for initialization
+	void Start ()
+	{
+	    input = GetComponent<HFTInput>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    transform.position += Joystick * Time.deltaTime;
+	}
+}
